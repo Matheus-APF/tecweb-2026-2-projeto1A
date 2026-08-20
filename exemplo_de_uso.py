a2b16e4c2abc0575da2707185b04829d4eb543ac
Matheus Fernandes <matheusapf@al.insper.edu.br>
2026-08-20 09:20:51 -0300

    substituicao e adpatacao dados json por sql

--- exemplo_de_uso.py
+++ exemplo_de_uso.py
@@ -1,10 +1,17 @@
 from database import Database
 from database import Note
+import json
 
 db = Database('banco')
 
-db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
-db.add(Note(title=None, content='Lembrar de tomar água'))
+def load_data(name):
+    return json.load(open(f'data/{name}', encoding='utf-8'))  
+
+for note in load_data('notes.json'):
+    db.add(Note(title=note['titulo'], content=note['detalhes']))
+
+#db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
+#db.add(Note(title=None, content='Lembrar de tomar água'))
 
 notes = db.get_all()
 for note in notes:
--- views.py
+++ views.py
@@ -1,8 +1,21 @@
 from utils import add_data, load_data, load_template, build_response
 from urllib.parse import unquote_plus
+import database 
 
-
+# Tratar requests
 def index(request):
+    # Adiciona cartas do banco de dados ao html
+    sql_entity = database.Database('data/banco')
+    notes_li = []
+    for dados in sql_entity.get_all():
+        notes_li.append(
+            load_template('components/note.html').format(
+                title=dados.title,
+                details=dados.content
+            )
+        )
+    notes = '\n'.join(notes_li)
+
     # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
     if request.startswith('POST'): # Se vor Post
         request = request.replace('\r', '')  # Remove caracteres indesejados
@@ -13,11 +26,12 @@ def index(request):
         for chave_valor in corpo.split('&'):
             chave_valor = chave_valor.split('=')
             params[chave_valor[0]] = unquote_plus(chave_valor[1])     # subtstitui caract espec auto       
-        add_data({'titulo': params['titulo'], 'detalhes': params['detalhes']})
+
+        note = database.Note(title=params['titulo'], content=params['detalhes'])
+        sql_entity.add(note)
+
+        #add_data({'titulo': params['titulo'], 'detalhes': params['detalhes']})
 
         return build_response(code=303, reason='See Other', headers='Location: /')
 
-    
-    notes_li = [load_template('components/note.html').format(title=dados['titulo'], details=dados['detalhes']) for dados in load_data('notes.json')]
-    notes = '\n'.join(notes_li)
     return build_response(load_template('index.html').format(notes=notes))
